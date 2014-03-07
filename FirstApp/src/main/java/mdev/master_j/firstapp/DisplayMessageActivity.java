138682fc3b0463505f082cf0e41541898b5d789f
Igor B <masterj63@gmail.com>
2014-03-08 03:52:00 +0400

    upgraded DisplayMessageActivity. Support library still doesnt work

--- FirstApp/src/main/java/mdev/master_j/firstapp/DisplayMessageActivity.java
+++ FirstApp/src/main/java/mdev/master_j/firstapp/DisplayMessageActivity.java
@@ -1,6 +1,9 @@
 package mdev.master_j.firstapp;
 
+//import android.support.v4.app.NavUtils;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,10 +11,16 @@ import android.view.MenuItem;
 
 public class DisplayMessageActivity extends Activity {
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
@@ -25,12 +34,10 @@ public class DisplayMessageActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //NavUtils.navigateUpFromSameTask(this);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
