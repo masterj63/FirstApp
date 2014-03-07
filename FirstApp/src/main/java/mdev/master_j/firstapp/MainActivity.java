07577bbd67c99baf9ce4eb3a412bcfbbb7508a86
Igor B <masterj63@gmail.com>
2014-03-08 03:29:08 +0400

    added new activity invoking on button press

--- FirstApp/src/main/java/mdev/master_j/firstapp/MainActivity.java
+++ FirstApp/src/main/java/mdev/master_j/firstapp/MainActivity.java
@@ -1,13 +1,16 @@
 package mdev.master_j.firstapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,10 @@ public class MainActivity extends Activity {
     }
 
     public void sendMessage(View view) {
-        //empty for now
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
